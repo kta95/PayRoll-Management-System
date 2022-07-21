1af249326857a150c65cee803f5023cbdcd69020
Kyaw Thet Aung <billionairekta@gmail.com>
2022-07-21 10:45:57 +0630

    renovated homepage

--- src/forms/views/HomeForm.java
+++ src/forms/views/HomeForm.java
@@ -5,8 +5,24 @@ import java.awt.EventQueue;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.SwingConstants;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
+
+import entities.Department;
+import entities.Employee;
+import entities.Payroll;
+import entities.Position;
+import services.AllowanceService;
+import services.AttendanceService;
+import services.DeductionService;
+import services.DepartmentService;
+import services.EmployeeService;
+import services.PayrollService;
+import services.PositionService;
+
 import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
@@ -17,6 +33,18 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
 
 public class HomeForm extends JInternalFrame {
+
+	private EmployeeService employeeService;
+	private AttendanceService attendanceService;
+	private AllowanceService allowanceService;
+	private DeductionService deductionService;
+	private PayrollService payrollService;
+	private DepartmentService departmentService;
+	private PositionService positionService;
+	List<Employee> employeeList = new ArrayList<>();
+	List<Department> deptList = new ArrayList<>();
+	List<Position> positionList = new ArrayList<>();
+	List<Payroll> payrollList = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -38,8 +66,30 @@ public class HomeForm extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public HomeForm() {
+		initializeDependancy();
+
+		initialize();
+
+	}
+	
+	private void initializeDependancy() {
+
+		this.employeeService = new EmployeeService();
+		this.attendanceService = new AttendanceService();
+		this.allowanceService = new AllowanceService();
+		this.deductionService = new DeductionService();
+		this.payrollService = new PayrollService();
+		this.departmentService = new DepartmentService();
+		this.positionService = new PositionService();
+	}
+	private void initialize() {
 		getContentPane().setBackground(Color.WHITE);
 		getContentPane().setLayout(null);
+		
+		employeeList = employeeService.findAllEmployees();
+		deptList = departmentService.findAllDepartments();
+		positionList = positionService.findAllPositions();
+		payrollList = payrollService.findAllPayrolls();
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -60,6 +110,21 @@ public class HomeForm extends JInternalFrame {
 		lblNewLabel.setBounds(10, 11, 328, 67);
 		panel_1.add(lblNewLabel);
 		
+		JLabel lblNewLabel_1 = new JLabel("Total Employee :");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(37, 89, 179, 67);
+		panel_1.add(lblNewLabel_1);
+		
+		
+		
+		JLabel empCount = new JLabel("");
+		empCount.setHorizontalAlignment(SwingConstants.CENTER);
+		empCount.setText(employeeList.size() + "");
+		empCount.setFont(new Font("Tahoma", Font.BOLD, 18));
+		empCount.setBounds(214, 89, 69, 67);
+		panel_1.add(empCount);
+		
 		JPanel panel_1_1 = new JPanel();
 		panel_1_1.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_1_1.setBounds(498, 45, 348, 183);
@@ -72,11 +137,43 @@ public class HomeForm extends JInternalFrame {
 		lblDepartment.setBounds(10, 11, 328, 67);
 		panel_1_1.add(lblDepartment);
 		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Total Payroll :");
+		lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_1_1.setBounds(55, 89, 179, 67);
+		panel_1_1.add(lblNewLabel_1_1_1);
+		
+		JLabel payrollCount = new JLabel("");
+		payrollCount.setHorizontalAlignment(SwingConstants.CENTER);
+		payrollCount.setFont(new Font("Tahoma", Font.BOLD, 18));
+		payrollCount.setBounds(220, 89, 69, 67);
+		panel_1_1.add(payrollCount);
+		
+		payrollCount.setText(payrollList.size() + "");
 		JPanel panel_1_2 = new JPanel();
 		panel_1_2.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_1_2.setBounds(498, 281, 348, 183);
 		panel.add(panel_1_2);
 		panel_1_2.setLayout(null);
+		
+		JLabel lblPosition = new JLabel("Position");
+		lblPosition.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPosition.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblPosition.setBounds(10, 11, 328, 67);
+		panel_1_2.add(lblPosition);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Total Position :");
+		lblNewLabel_1_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_1_1_1.setBounds(59, 89, 179, 67);
+		panel_1_2.add(lblNewLabel_1_1_1_1);
+		
+		JLabel positionCount = new JLabel("");
+		positionCount.setHorizontalAlignment(SwingConstants.CENTER);
+		positionCount.setFont(new Font("Tahoma", Font.BOLD, 18));
+		positionCount.setBounds(227, 89, 69, 67);
+		panel_1_2.add(positionCount);
+		positionCount.setText(positionList.size() + "");
 		
 		JPanel panel_1_3 = new JPanel();
 		panel_1_3.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -84,12 +181,30 @@ public class HomeForm extends JInternalFrame {
 		panel.add(panel_1_3);
 		panel_1_3.setLayout(null);
 		
+		JLabel lblDepartment_1 = new JLabel("Department");
+		lblDepartment_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDepartment_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblDepartment_1.setBounds(10, 11, 328, 67);
+		panel_1_3.add(lblDepartment_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Total Department :");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_1.setBounds(33, 89, 188, 67);
+		panel_1_3.add(lblNewLabel_1_1);
+		
+		JLabel deptCount = new JLabel("");
+		deptCount.setHorizontalAlignment(SwingConstants.CENTER);
+		deptCount.setFont(new Font("Tahoma", Font.BOLD, 18));
+		deptCount.setBounds(218, 89, 69, 67);
+		panel_1_3.add(deptCount);
+		
+		deptCount.setText(deptList.size() + "");
 		this.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
 		BasicInternalFrameUI ui= (BasicInternalFrameUI)this.getUI();
 		ui.setNorthPane(null);
 		
 		
 		setBounds(0, 0, 976, 591);
-
 	}
 }
